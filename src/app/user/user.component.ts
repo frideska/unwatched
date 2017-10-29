3d9d52ff66602c9b15b92049321bf99e134f54f4
oleast <oleast@stud.ntnu.no>
2017-10-29 14:05:07 +0100

    Setting up Angular to be served from express backend

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,29 @@
+import { HttpClient } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core'
+
+class GoogleUser {
+    image: string
+    name: string
+    admin: boolean
+    id: string
+    date: string
+    updated: string
+    email: string
+    token: string
+
+    constructor(user) {
+        this.image = user.google.image
+        this.name = user.google.name
+        this.admin = user.admin
+        this.id = user.google.id
+        this.date = user.date
+        this.updated = user.updated
+        this.email = user.google.email
+        this.token = user.google.token
+    }
+}
+
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +32,14 @@ import { Component, OnInit } from '@angular/core'
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+    private user: GoogleUser
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpClient) { }
 
+    ngOnInit(): void {
+       this.http.get('/auth/profile').subscribe((data) => {
+           this.user = new GoogleUser(data)
+           console.log(this.user)
+       })
+    }
 }
