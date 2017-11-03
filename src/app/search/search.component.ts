e562ca2762094c30f682926c44343874f31cb82e
Paalch <pciverse@stud.ntnu.no>
2017-11-03 15:37:22 +0100

    Added test search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,14 +8,16 @@ import { SearchService } from './search.service'
 })
 export class SearchComponent implements OnInit {
   result: any
-  keyword: String
+  titles: string[]
+  keyword: string
   constructor( private searchService: SearchService) { }
 
   ngOnInit() {
 
   }
   async onChange() {
+    this.keyword = 'The dark night'
     this.result = await this.searchService.getSearch(this.keyword)
+    this.titles = this.result.results.map((result) => result.title)
   }
-
 }
