bf1322f918cede5763115acfa646fd36857ae87e
Unknown <pciverse@stud.ntnu.no>
2017-11-04 21:43:57 +0100

    Added watchlist service

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { WatchlistService } from './watchlist.service'
 
 @Component({
   selector: 'app-watchlist',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatchlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private watchlistService: WatchlistService) {}
 
   ngOnInit() {
+    this.watchlistService.addMovieToWatchlist('test')
   }
+
 
 }
