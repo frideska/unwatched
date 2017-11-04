dd420906c7c83833a80955b8fe68468f983c1036
tsm121 <tsm121@gmail.com>
2017-11-04 16:32:59 +0100

    Extracting relevant search information from and into a JSON obj

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { SearchService } from './search.service'
 import { NgModel } from '@angular/forms'
+import {isNullOrUndefined, isUndefined} from "util";
 
 @Component({
   selector: 'app-search',
@@ -8,19 +9,38 @@ import { NgModel } from '@angular/forms'
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  result: any
-  titles: string[]
+  JSONresult: any
+  searchResult: string
   keyword: string
   constructor( private searchService: SearchService) { }
 
   ngOnInit() {
 
   }
+
+
   async onChange(event) {
     this.keyword = event
+    let missingPosterSrc = "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png"
+    let posterPath = "https://image.tmdb.org/t/p/w1280/"
     if (this.keyword) {
-      this.result = await this.searchService.getSearch(this.keyword)
-      this.titles = this.result.results.map((result) => result.title)
+      this.JSONresult = await this.searchService.getSearch(this.keyword)
+
+      //JSON for searchResult
+      this.searchResult = this.JSONresult.results.map((result) => (
+        {
+          movieID: result.id,
+          mediaType: result.media_type,
+          video: isNullOrUndefined(result.video) ? false : result.video,
+          title: isNullOrUndefined(result.title) ? result.name : result.title,
+          content: isNullOrUndefined(result.overview) ? "No description" : ((result.overview.length <= 30) ? result.overview : result.overview.substring(0, 150).concat(" (...)")),
+          imgSrc: isNullOrUndefined(result.poster_path) ? (missingPosterSrc) : (posterPath).concat(result.poster_path),
+          rating: result.vote_average,
+          genre: result.genre_ids,
+          year: isNullOrUndefined(result.release_date) ? result.release_date : "("+result.release_date.substring(0, 4)+")"
+        }))
+
+      console.log(this.searchResult)
     }
   }
 }
