625e5fcbb104de1ff33f4eba25bda0e020bc6f28
oleast <oleast@stud.ntnu.no>
2017-11-04 21:17:28 +0100

    Adding functional search and routing based on the input field in the navbar

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { SearchService } from './search.service'
 import { NgModel } from '@angular/forms'
-import {isNullOrUndefined, isUndefined} from "util";
 
 @Component({
   selector: 'app-search',
@@ -9,38 +8,12 @@ import {isNullOrUndefined, isUndefined} from "util";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  JSONresult: any
-  searchResult: string
-  keyword: string
   constructor( private searchService: SearchService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-
-  async onChange(event) {
-    this.keyword = event
-    let missingPosterSrc = "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png"
-    let posterPath = "https://image.tmdb.org/t/p/w1280/"
-    if (this.keyword) {
-      this.JSONresult = await this.searchService.getSearch(this.keyword)
-
-      //JSON for searchResult
-      this.searchResult = this.JSONresult.results.map((result) => (
-        {
-          movieID: result.id,
-          mediaType: result.media_type,
-          video: isNullOrUndefined(result.video) ? false : result.video,
-          title: isNullOrUndefined(result.title) ? result.name : result.title,
-          content: isNullOrUndefined(result.overview) ? "No description" : ((result.overview.length <= 30) ? result.overview : result.overview.substring(0, 150).concat(" (...)")),
-          imgSrc: isNullOrUndefined(result.poster_path) ? (missingPosterSrc) : (posterPath).concat(result.poster_path),
-          rating: result.vote_average,
-          genre: result.genre_ids,
-          year: isNullOrUndefined(result.release_date) ? result.release_date : "("+result.release_date.substring(0, 4)+")"
-        }))
-
-      console.log(this.searchResult)
-    }
+  onChange(event) {
+    this.searchService.setQuery(event)
+    this.searchService.search()
   }
 }
