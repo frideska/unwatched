daa32b9c76882c4ed609ace8f409e4b498b93108
oleast <oleast@stud.ntnu.no>
2017-11-05 17:32:24 +0100

    Adding simple version check to make sure node supports async/await

--- server/app.js
+++ server/app.js
@@ -4,6 +4,22 @@
  */
 
 /**
+ * Make sure node version supports async/await.
+ * If not, the program will fail with error suggesting async syntax is wrong.
+ */
+const NODE_VERSION = process.versions.node
+const SUPPORTED_NODE_VERSION = '8.9.0'
+
+if (NODE_VERSION.charAt(0) < 8) {
+    console.log(`
+        You are running an outdated version of Nodejs!
+        Nodejs version ${NODE_VERSION} is NOT supported.
+        Please upgrade to at least version ${SUPPORTED_NODE_VERSION}!
+    `)
+    process.exit(1)
+}
+
+/**
  * Get external dependencies
  */
 const express = require('express')
