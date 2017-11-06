1f9755b5b0117ec3ff70a25fc5c64d90942356da
Unknown <pciverse@stud.ntnu.no>
2017-11-06 17:11:09 +0100

    Added middleware to check that user is logged in

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,15 @@
 const api = require('express').Router()
+const express = require('../../express')
 
 const tmdb = require('./tmdb')
 const watchlist = require('./watchlist')
 
+api.use(express.loggedIn)
+
 api.get('/', (req, res) => {
     res.send({hello: 'world!'})
 })
+
 
 api.use('/tmdb', tmdb)
 api.use('/watchlist', watchlist)
