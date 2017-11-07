9a13813b1dbf425a4b8433b5e62a519fd6909335
Unknown <pciverse@stud.ntnu.no>
2017-11-07 15:15:48 +0100

    Getting watchlist now works

--- server/db/controllers/MovieController.js
+++ server/db/controllers/MovieController.js
@@ -2,27 +2,65 @@ const UserMovie = require('../models/UserMovie')
 const User = require('../models/User')
 const tmdbWrapper = require('../../tmdb/')
 
+/**
+ * Creates a new entry in the UserMovie collection
+ * with a given movie_id and user._id
+ * @param movieID
+ * @param user
+ * @returns {Promise.<boolean>}
+ */
 let newMovie = async (movieID, user) => {
-  let movie = UserMovie.findOne({movie_id: movieID.toString(), user: user._id})
-  console.log(user)
-  if(!movie){
+  let movie = await UserMovie.findOne({movie_id: movieID, user_id: user._id})
+  try {
     let userMovie = new UserMovie()
     userMovie.movie_id = movieID
-    userMovie.user = user._id
-    userMovie.save((err) =>  {
-        if (err) return handleError(err);
-      })
+    userMovie.user_id = user._id
+    userMovie.save()
+    return true
+  }
+  catch(err) {
+    console.error(error)
+    return false
+  }
+  }
+
+
+/**
+ * Findes all the movies the given user have in his/hers watchlist
+ * @param user
+ * @returns {Promise.<*[]>}
+ */
+let findMovieForUser = async (user) => {
+  //Findes all the movies, that are in the watchlist of the current user
+  let userMovies = await UserMovie.find({user_id: user._id})
+  //For each id in the UserMovie database, we return all the informasjon about the movie, we use
+  //promise all to make sure that the array is not returned while pending
+  try {
+    return await Promise.all(userMovies.map(movie => tmdbWrapper.details(movie.movie_id.toString())))
+  }catch (err) {
+    console.log(err)
+    return false
   }
 }
 
-let findMovieForUser = async (user) => {
-  let usermovies = await UserMovie.find({})
-  return usermovies.map(movie => tmdbWrapper.details(movie.movie_id))
+/**
+ * Removes all movies with the given MovieID and user
+ * @param movieID
+ * @param user
+ * @returns {Promise.<boolean>}
+ */
+let removeMovieForUser = async (movieID, user) => {
+  try {
+    await UserMovie.remove({movie_id: movieID, user_id: user._id})
+  } catch (err) {
+      console.log(err)
+      return false
+    }
+  return true
 }
-
-
 
 module.exports = {
   newMovie,
-  findMovieForUser
+  findMovieForUser,
+  removeMovieForUser
 }
