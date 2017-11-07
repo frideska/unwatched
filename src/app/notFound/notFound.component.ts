4f0733fd296e525aec8c9d0c4e81103612452694
Unknown <pciverse@stud.ntnu.no>
2017-11-07 21:50:12 +0100

    Added 404 page

---
+++ src/app/notFound/notFound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notFound.component.html',
+  styleUrls: ['./notFound.component.css']
+})
+export class NotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
