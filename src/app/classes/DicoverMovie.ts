4553721e91840c0e32af5c7032226c4c6af13964
frideska <frideskarz28@gmail.com>
2017-11-09 14:09:28 +0100

    Displays movies under 'discover movies'

--- src/app/classes/DicoverMovie.ts
+++ src/app/classes/DicoverMovie.ts
@@ -5,14 +5,16 @@ export class DiscoverMovie {
     public genres: number[]
     public overview: string
     public poster: string
+    public poster2: string
     public releaseDate: string
     public title: string
     public score: number
 
-    constructor({ genre_ids, overview, backdrop_path, release_date, title, vote_average }) {
+    constructor({ genre_ids, overview, backdrop_path, poster_path, release_date, title, vote_average }) {
         this.genres = genre_ids
         this.overview = overview
         this.poster = POSTER_URL + backdrop_path
+        this.poster2 = POSTER_URL + poster_path
         this.releaseDate = release_date
         this.title = title
         this.score = vote_average
