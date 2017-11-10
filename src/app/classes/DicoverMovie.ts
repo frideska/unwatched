1d9c5fba0e2b7ffd73a125ca2d0897d8a7c238b8
frideska <frideskarz28@gmail.com>
2017-11-10 12:40:31 +0100

    Small changes for cards in home

--- src/app/classes/DicoverMovie.ts
+++ src/app/classes/DicoverMovie.ts
@@ -4,6 +4,7 @@ const POSTER_URL = 'https://image.tmdb.org/t/p/w1280/'
 export class DiscoverMovie {
     public genres: number[]
     public overview: string
+    public overview2: string
     public poster: string
     public poster2: string
     public releaseDate: string
@@ -12,10 +13,11 @@ export class DiscoverMovie {
 
     constructor({ genre_ids, overview, backdrop_path, poster_path, release_date, title, vote_average }) {
         this.genres = genre_ids
-        this.overview = overview
+        this.overview2 = (overview.length <= 30) ? overview : overview.substring(0, 150).concat(' (...)')
+        this.overview = overview    
         this.poster = POSTER_URL + backdrop_path
         this.poster2 = POSTER_URL + poster_path
-        this.releaseDate = release_date
+        this.releaseDate = release_date ? '(' + release_date.substring(0, 4) + ')' : release_date
         this.title = title
         this.score = vote_average
     }
