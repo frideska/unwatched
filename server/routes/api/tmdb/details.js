74bce3ff30ab29afbd56cdcafafb4fde964f2704
Paalch <pciverse@stud.ntnu.no>
2017-11-10 14:29:52 +0100

    Updated tmdb api

--- server/routes/api/tmdb/details.js
+++ server/routes/api/tmdb/details.js
@@ -2,8 +2,11 @@ const route = require('express').Router()
 
 const tmdbWrapper = require('../../../tmdb/')
 
-route.get('/:id', async (req, res) => {
-  res.send(await tmdbWrapper.details(req.params.id))
+route.get('/movie/:id', async (req, res) => {
+  res.send(await tmdbWrapper.details.movieDetails(req.params.id))
+})
+route.get('/tv/:id', async (req, res) => {
+  res.send(await tmdbWrapper.details.tvDetails(req.params.id))
 })
 
 module.exports = route
