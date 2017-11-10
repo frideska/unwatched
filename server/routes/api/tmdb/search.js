59fcb006d700d15607aca42dee7f14eb8c40e39e
oleast <oleast@stud.ntnu.no>
2017-11-10 14:43:37 +0100

    Adding classes for frontend searches and basing cards on them

--- server/routes/api/tmdb/search.js
+++
@@ -1,9 +0,0 @@
-const route = require('express').Router()
-
-const tmdbWrapper = require('../../../tmdb')
-
-route.get('/:q', async (req, res) => {
-    res.send(await tmdbWrapper.search(req.params.q))
-})
-
-module.exports = route
