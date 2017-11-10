d2f24d0ce2a042948da516520bec3af806b09b07
Ole Anders Stokker <oleast@stud.ntnu.no>
2017-11-10 15:30:11 +0100

    Indented correctly, fixed tv_id

--- server/tmdb/details.js
+++ server/tmdb/details.js
@@ -1,29 +1,25 @@
 const tvDetails = async (id) => {
     try {
-        return await tmdb.tv.details(
-            {
-              movie_id: id || ''
-            }
-        )
+        return await tmdb.tv.details({
+            tv_id: id || ''
+        })
     } catch (err) {
         console.error(err)
-      return null
+        return null
     }
 }
 const movieDetails = async (id) => {
-  try {
-    return await tmdb.movie.details(
-      {
-        movie_id: id || ''
-      }
-    )
-  } catch (err) {
-    console.error(err)
-    return null
-  }
+    try {
+        return await tmdb.movie.details({
+            movie_id: id || ''
+        })
+    } catch (err) {
+        console.error(err)
+        return null
+    }
 }
 
 module.exports = {
-  movieDetails,
-  tvDetails
+    movieDetails,
+    tvDetails
 }
