1fb594e028587a290fcc77a53c4098c402fb36fd
Unknown <pciverse@stud.ntnu.no>
2017-11-11 15:59:51 +0100

    Added standard card

---
+++ src/app/components/cards/standard-card/standard-card.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input, OnInit} from '@angular/core'
+
+import { WatchlistService} from 'services/watchlist.service'
+import { LibraryService } from 'services/library.service'
+import { CardElement } from '../../../classes/CardElement'
+
+@Component({
+  selector: 'app-standard-card',
+  templateUrl: './standard-card.component.html',
+  styleUrls: ['../card.component.css']
+})
+export class StandardCardComponent implements OnInit {
+  @Input() element: CardElement
+
+  constructor(private watchlistService: WatchlistService, private libraryService: LibraryService) { }
+
+  ngOnInit() {
+  }
+
+  addToWatchlist() {
+    if (this.element.watchlist) {
+      this.watchlistService.removeMovieFromWatchlist(this.element.id)
+    } else  {
+      this.watchlistService.addMovieToWatchlist(this.element.id)
+    }
+  }
+  addToLibrary() {
+    if (this.element.library) {
+      this.libraryService.removeMovieFromLibrary(this.element.id)
+    } else  {
+      this.libraryService.addMovieToLibrary(this.element.id)
+    }
+  }
+}
