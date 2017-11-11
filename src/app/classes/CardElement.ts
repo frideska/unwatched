675b12e371201a55bf2e499a492102f54e198a19
Unknown <pciverse@stud.ntnu.no>
2017-11-11 15:59:51 +0100

    Added standard card

--- src/app/classes/CardElement.ts
+++ src/app/classes/CardElement.ts
@@ -1,9 +1,4 @@
-
-import { SearchElement } from 'classes/SearchElement'
-
-const MISSING_PATH = 'http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
-const PREVIEW_IMG_PATH = 'https://image.tmdb.org/t/p/w185'
-const POSTER_PATH = 'https://image.tmdb.org/t/p/w1280'
+const POSTER_URL = 'https://image.tmdb.org/t/p/w1280/'
 
 export class CardElement {
     public id: string
@@ -18,15 +13,15 @@ export class CardElement {
     public date: Date
     public type: string
 
-    constructor({id, title, overview, poster, backdrop_path, vote_average, genres, release_date, watchlist, library, media_type}) {
+    constructor({id, title, overview, poster_path, backdrop_path, vote_average, genres, release_date, watchlist, library, media_type}) {
 
         const o = overview || 'No description'
 
         this.id = id
         this.title = title
         this.overview = (o.length <= 30) ? o : o.substring(0, 150).concat(' (...)')
-        this.poster = poster ? PREVIEW_IMG_PATH.concat(poster) : MISSING_PATH
-        this.backdropPath = backdrop_path ? POSTER_PATH.concat(poster) : MISSING_PATH
+        this.poster = POSTER_URL + poster_path
+        this.backdropPath = POSTER_URL + backdrop_path
         this.rating = vote_average
         this.genres = genres
         this.watchlist = watchlist
