21b8cf738d0719baca8240d7c0d9dd3bf4b681b2
tsm121 <tsm121@gmail.com>
2017-11-11 02:29:52 +0100

    Added movie_type and fixed wrong api call

--- src/app/services/watchlist.service.ts
+++ src/app/services/watchlist.service.ts
@@ -7,6 +7,7 @@ import { SearchMovie } from 'classes/SearchMovie'
 @Injectable()
 export class WatchlistService {
   private URL = '/api/watchlist'
+  private findURL = 'api/tmdb/find'
   watchlist: any
 
   constructor(private http: Http) {}
@@ -27,9 +28,9 @@ export class WatchlistService {
   }
   public async getWatchlist() {
     try {
-      const response = await this.http.get(this.URL + '/movie').toPromise()
+      const url = `${this.findURL}/${'tt0100507'}?`
+      const response = await this.http.get(url).toPromise()
       if (response.status === 200) {
-        console.log(response)
         this.reconfigure(response.json())
       }
     }catch (err) {
@@ -38,6 +39,7 @@ export class WatchlistService {
   }
 
   private reconfigure(json) {
+    console.log(json.movie_results[0])
     this.watchlist = json.map((result) => {
       switch (result.media_type) {
         case 'movie': return new SearchMovie(result)
@@ -45,6 +47,5 @@ export class WatchlistService {
       }
     })
 
-    console.log(this.watchlist)
   }
 }
