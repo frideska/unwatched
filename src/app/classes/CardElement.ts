c22da0fc01e89a65d0c532a163fb1cf3c8860d7b
tsm121 <tsm121@gmail.com>
2017-11-15 17:20:21 +0100

    Fixed showing preview img in CardElement. Added img, desc and some styling to listView

--- src/app/classes/CardElement.ts
+++ src/app/classes/CardElement.ts
@@ -1,4 +1,7 @@
 const POSTER_URL = 'https://image.tmdb.org/t/p/w1280/'
+const PREVIEW_IMG_PATH = 'https://image.tmdb.org/t/p/w185'
+const MISSING_PATH = 'http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+
 
 export class CardElement {
     public id: string
@@ -6,6 +9,7 @@ export class CardElement {
     public genres: string[]
     public overview: string
     public backdropPath: string
+    public image: string
     public poster: string
     public rating: number
     public watchlist: boolean
@@ -20,6 +24,7 @@ export class CardElement {
         this.id = id
         this.title = title
         this.overview = (o.length <= 30) ? o : o.substring(0, 150).concat(' (...)')
+        this.image = poster_path ? PREVIEW_IMG_PATH.concat(poster_path) : MISSING_PATH
         this.poster = POSTER_URL + poster_path
         this.backdropPath = POSTER_URL + backdrop_path
         this.rating = vote_average
