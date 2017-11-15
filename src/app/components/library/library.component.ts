e1055a4c80b9439aef48d30680dcd478b4e2ec3c
tsm121 <tsm121@gmail.com>
2017-11-15 14:41:20 +0100

    Fixed date to show only year. Added dynamic rendring of listView

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { LibraryService } from 'services/library.service'
 
+
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html'
