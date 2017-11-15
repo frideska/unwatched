ba81ad34851af42848279de521942a4f78f5d562
Paalch <pciverse@stud.ntnu.no>
2017-11-15 21:52:47 +0100

    Fixed tests

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -1,25 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FilterComponent } from './filter.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { FilterComponent } from './filter.component'
+import { RouterTestingModule } from '@angular/router/testing'
+import { LibraryService } from '../../services/library.service'
+import { HttpModule } from '@angular/http'
 
 describe('FilterComponent', () => {
-  let component: FilterComponent;
-  let fixture: ComponentFixture<FilterComponent>;
+  let component: FilterComponent
+  let fixture: ComponentFixture<FilterComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpModule
+      ], declarations: [ FilterComponent ],
+      providers: [
+        LibraryService
+      ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(FilterComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
