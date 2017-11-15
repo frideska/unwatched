b4b80ab8a821b03ef9f292d4fc1157ba5ab9b689
frideska <frideskarz28@gmail.com>
2017-11-15 14:33:27 +0100

    Changed home to fit cardElement

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
 import { DiscoverService } from 'services/discover.service'
-import { DiscoverMovie } from 'classes/DicoverMovie'
+import { CardElement } from 'classes/CardElement'
 
 @Component({
   selector: 'app-home',
@@ -10,39 +10,55 @@ import { DiscoverMovie } from 'classes/DicoverMovie'
 })
 export class HomeComponent implements OnInit {
 
-  public movies: DiscoverMovie[]
-  public moviesSliced: DiscoverMovie[]
+  public movies: CardElement[]
+  public moviesSliced: CardElement[]
+  public tv: CardElement[]
+  public tvSliced: CardElement[]
   public value1: number
   public value2: number
   public movieCard: any
   public tvCard: any
 
   constructor(private discover: DiscoverService) {
-    //this.movieCard = document.getElementById("movieCard")
-    //this.tvCard = document.getElementById("tvCard")
   }
 
   async ngOnInit() {
-    this.movies = await this.discover.getDiscovery()
+    this.movies = await this.discover.getDiscoveryMovie()
+    this.tv = await this.discover.getDiscoveryTV()
+    console.log(this.movies)
+    console.log(this.tv)
     this.value1 = 0
     this.value2 = 2
-    this.showObjects(this.value1, this.value2)
+    this.showMovies(this.value1, this.value2)
+    this.showTV(this.value1, this.value2)
   }
 
-  showObjects(value1, value2) {
+  showMovies(value1, value2) {
     this.moviesSliced = this.movies.slice(value1, value2)
   }
 
-  nextObject() {
-    this.value1 < this.movies.length - 3 ? this.value1 += 2 : this.value1 = 0
-    this.value2 < this.movies.length - 1 ? this.value2 += 2 : this.value2 = 2
-    this.showObjects(this.value1, this.value2)
+  showTV(value1, value2){
+    this.tvSliced = this.tv.slice(value1, value2)
   }
 
-  prevObject() {
-    this.value1 > 0 ? this.value1 -= 2 : this.value1 = this.movies.length - 3
-    this.value2 > 2 ? this.value2 -= 2 : this.value2 = this.movies.length - 1
-    this.showObjects(this.value1, this.value2)
+  nextObject(condition) {
+    this.value1 < this.movies.length - 3 ? this.value1 += 2 : this.value1 = 0
+    this.value2 < this.movies.length - 1 ? this.value2 += 2 : this.value2 = 2
+    if(condition='movie'){
+      this.showMovies(this.value1, this.value2)
+    }else if(condition='movie'){
+      this.showTV(this.value1, this.value2)
+    }
+  }
+
+  prevObject(condition) {
+    this.value1 > 0 ? this.value1 -= 2 : this.value1 = this.movies.length - 4
+    this.value2 > 2 ? this.value2 -= 2 : this.value2 = this.movies.length - 2
+    if(condition='tv'){
+      this.showMovies(this.value1, this.value2)
+    }else if(condition='tv'){
+      this.showTV(this.value1, this.value2)
+    }
   }
 
 }
