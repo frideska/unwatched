a2d277f4a2fdedeef6cbeb0e13268c9e61cbc5e6
frideska <frideskarz28@gmail.com>
2017-11-15 14:32:52 +0100

    Merged new cardElement and changed service

--- src/app/services/discover.service.ts
+++ src/app/services/discover.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
 
-import { DiscoverMovie } from 'classes/DicoverMovie'
+import { CardElement } from 'classes/CardElement'
 
 @Injectable()
 export class DiscoverService {
-  private discoveryURL = '/api/tmdb/discover/movie'
+  private discoveryURLmovie = '/api/tmdb/discover/movie'
+  private discoveryURLtv = '/api/tmdb/discover/tv'
 
   constructor(private http: Http) {}
 
-  public async getDiscovery(): Promise<any> {
+  public async getDiscoveryMovie(): Promise<any> {
     try {
-      const response = await this.http.get(this.discoveryURL).toPromise()
-      return response.json().results.map((movie) => new DiscoverMovie(movie))
+      const response = await this.http.get(this.discoveryURLmovie).toPromise()
+      return response.json().map((movie) => new CardElement(movie))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  public async getDiscoveryTV(): Promise<any> {
+    try {
+      const response = await this.http.get(this.discoveryURLtv).toPromise()
+      return response.json().map((tv) => new CardElement(tv))
     } catch (err) {
       console.error(err)
     }
