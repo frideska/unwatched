8dc4a36e4f288ce77c95ff5e4f28563f67b482ca
Paalch <pciverse@stud.ntnu.no>
2017-11-16 11:43:22 +0100

    fixed spelling

--- server/tmdb/discover/index.js
+++ server/tmdb/discover/index.js
@@ -37,7 +37,7 @@
 
 
 
-const GenreController = require('../../db/Controllers/GenreController')
+const GenreController = require('../../db/controllers/GenreController')
 
 const movie = async () => {
     try {
