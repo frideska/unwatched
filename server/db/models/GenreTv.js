ea5bec01b5bffb7eb24439ef999d84626b964574
oleast <oleast@stud.ntnu.no>
2017-11-17 14:22:49 +0100

    Fixed unhandled promise rejection

--- server/db/models/GenreTv.js
+++ server/db/models/GenreTv.js
@@ -6,7 +6,7 @@ let Schema = mongoose.Schema
  * Define Shema for Movie
  */
 let GenreSchema = new Schema({
-  id: { type: Number, required: true, unique: true },
+  tv_id: { type: Number, required: true },
   name: { type: String }
 })
 
