a0c6a235df24cf362bea495c7df6a68feae341a7
oleast <oleast@stud.ntnu.no>
2017-11-19 14:10:42 +0100

    Fixed linting errors in frontend

--- src/app/components/card-light/card-light.component.spec.ts
+++ src/app/components/card-light/card-light.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { CardLightComponent } from './card-light.component';
+import { CardLightComponent } from './card-light.component'
 
 describe('CardLightComponent', () => {
-  let component: CardLightComponent;
-  let fixture: ComponentFixture<CardLightComponent>;
+  let component: CardLightComponent
+  let fixture: ComponentFixture<CardLightComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CardLightComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardLightComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CardLightComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
