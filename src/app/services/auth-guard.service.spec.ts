499848ed0ddb8bb0f74460fe22c88d63ca1f9e26
langokalla <martin.langmo.karlstrom@gmail.com>
2017-11-22 17:29:39 +0100

    AuthGuard spec

--- src/app/services/auth-guard.service.spec.ts
+++ src/app/services/auth-guard.service.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, inject } from '@angular/core/testing'
 
 import { AuthGuard } from './auth-guard.service'
+import { RouterTestingModule } from '@angular/router/testing'
+import {UserService} from './user.service'
+import { HttpModule } from '@angular/http'
 
 describe('AuthGuardService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [
+        RouterTestingModule,
+        HttpModule
+      ],
+      providers: [
+        AuthGuard,
+        UserService
+      ]
     })
   })
 
