551be50fe6b9c610cf8cfb761931bd7604f6a72a
tsm121 <tsm121@gmail.com>
2017-11-22 18:26:57 +0100

    Fixed styling and added routing url to 404

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,47 +1,18 @@
-import { Component, OnInit, ViewChild } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { AgWordCloudData, AgWordCloudDirective } from 'angular4-word-cloud'
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
+
 export class NotFoundComponent implements OnInit {
-
-  // WordCloudDirective
-  @ViewChild('word_cloud_chart') wordCloudChart: AgWordCloudDirective
-
-  // Main word cloud.
-  wordData: Array<AgWordCloudData> = [
-    {size: 3, text: 'NOT FOUND'},
-    {size: 1, text: ' '},
-    {size: 1, text: ' '},
-    {size: 2, text: this.router.url},
-    {size: 3, text: '404'}
-    ]
-
-  options = {
-    settings: {
-      minFontSize: 70,
-      maxFontSize: 150,
-    },
-    margin: {
-      top: 5,
-      right: 5,
-      bottom: 5,
-      left: 5
-    },
-    labels: false // false to hide hover labels
-  }
-
-  // Colors to use in cloud.
-  colors = ['#653399', '#982d84', '#b5d83c', '#e3d83f', '#00D377']
-
+  router_url = ''
   constructor( private router: Router ) { }
 
   ngOnInit() {
-    this.wordCloudChart.color = this.colors
+    this.router_url = this.router.url
   }
 
 }
