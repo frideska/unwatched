befdec3a50da462203d3bdbc3f26e8d986a1ac55
Paalch <pciverse@stud.ntnu.no>
2017-11-22 22:52:12 +0100

    Fixed tests that dose not work

--- src/app/services/library/library.service.spec.ts
+++ src/app/services/library/library.service.spec.ts
@@ -3,32 +3,22 @@ import { TestBed, inject } from '@angular/core/testing'
 import { LibraryService } from 'services/library/library.service'
 import { HttpModule } from '@angular/http'
 
-class MockLibraryService {
-  
-}
-
 describe('LibraryService', () => {
-  let service: LibraryService
-  let htmlElement: HTMLElement
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpModule
-      ], providers: [[{
-        provide: LibraryService,
-        userClass: MockLibraryService
-      }]]
+      ],
+      providers: [
+        LibraryService
+      ]
     })
   })
 
   it('should be created', inject([LibraryService], (service: LibraryService) => {
     expect(service).toBeTruthy()
-  }))
 
-  it('should toggle list view button', () => {
-   expect(service.listView).toBeFalsy()
-   service.toggleListView()
-   expect(service.listView).toBeTruthy()
- })
+  }))
 })
