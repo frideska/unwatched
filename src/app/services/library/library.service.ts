c573cb814dee0424b0644dd065ec7bdedb9af26c
frideska <frideskarz28@gmail.com>
2017-11-22 21:06:36 +0100

    Test for library service

--- src/app/services/library/library.service.ts
+++ src/app/services/library/library.service.ts
@@ -126,7 +126,7 @@ export class LibraryService {
   /**
    * Toggles between listView and gridView.
    */
-  private toggleListView() {
+  toggleListView() {
     this.listView = !this.listView
   }
 
