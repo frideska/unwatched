778db2cdf3db228aa02bf19651dbedfac8346c17
oleast <oleast@stud.ntnu.no>
2017-11-22 23:48:09 +0100

    Lintint frontend! :D

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-import { FilterComponent } from  './Filter.component'
+import { FilterComponent } from './filter.component'
 import { RouterTestingModule } from '@angular/router/testing'
 import { LibraryService } from 'services/library/library.service'
 import { WatchlistService } from 'services/watchlist/watchlist.service'
