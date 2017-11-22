cbede354c7693892ace6919c1a8884f1443a85b8
oleast <oleast@stud.ntnu.no>
2017-11-22 11:15:52 +0100

    Adding back user deletion

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const router = require('express').Router()
 
+const UserController = require('../../pgdb/db/controllers/UserController')
+
 const profile = require('./profile')
 
 router.use('/profile', profile)
@@ -8,6 +10,15 @@ router.use('/profile', profile)
 router.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/')
+})
+
+router.get('/all', async (req, res) => {
+  try {
+    const users = await UserController.getAllUsers()
+    res.json(users)
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 router.get(
