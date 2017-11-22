c8d75810f3687fc6865a1130c32a0f98744b97a7
Paalch <pciverse@stud.ntnu.no>
2017-11-22 22:07:13 +0100

    First working cardElement test

--- src/app/components/cards/card.component.spec.ts
+++ src/app/components/cards/card.component.spec.ts
@@ -8,6 +8,7 @@ import { HistoryService } from 'services/history/history.service'
 import { SearchService } from 'services/search/search.service'
 import { UserService } from 'services/user/user.service'
 import { HttpModule } from '@angular/http'
+import {CardElement} from "../../classes/CardElement";
 
 describe('CardComponent', () => {
   let component: CardComponent
@@ -31,9 +32,26 @@ describe('CardComponent', () => {
     .compileComponents()
   }))
 
-  beforeEach(() => {
+  beforeEach( async () => {
     fixture = TestBed.createComponent(CardComponent)
     component = fixture.componentInstance
+    fixture.detectChanges()
+
+    const card = await new CardElement(
+      {id: 268,
+      title: 'Batman',
+      genres: [ 'Fantasy', 'Action'],
+      overview: 'The Dark Knight of Gotham City begins his war on crime with his first major enemy being ' +
+      'the clownishly homicidal Joker, who has seized control of Gotham’s underworld.',
+      backdrop_path: '/2blmxp2pr4BhwQr74AdCfwgfMOb.jpg',
+        poster_path: '/kBf3g9crrADGMc2AMAMlLBgSm2h.jpg',
+          release_date: '1989-06-23',
+          vote_average: 7,
+        watchlist: false,
+        library: false,
+        media_type: 'tv',
+  })
+    component.element = card
     fixture.detectChanges()
   })
 
