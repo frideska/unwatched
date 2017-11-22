7ab7d659272b83fea03f3d6861d623660e1e6e89
frideska <frideskarz28@gmail.com>
2017-11-22 22:32:15 +0100

    Som fixing in testing for services

--- src/app/services/library/library.service.spec.ts
+++ src/app/services/library/library.service.spec.ts
@@ -3,18 +3,22 @@ import { TestBed, inject } from '@angular/core/testing'
 import { LibraryService } from 'services/library/library.service'
 import { HttpModule } from '@angular/http'
 
+class MockLibraryService {
+  
+}
+
 describe('LibraryService', () => {
-  let service: LibraryService;
-  let htmlElement: HTMLElement;
+  let service: LibraryService
+  let htmlElement: HTMLElement
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpModule
-      ],
-      providers: [
-        LibraryService
-      ]
+      ], providers: [[{
+        provide: LibraryService,
+        userClass: MockLibraryService
+      }]]
     })
   })
 
