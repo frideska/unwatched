7ba280ca5cbdfa4cc61ff6a842daa84d25cd8678
frideska <frideskarz28@gmail.com>
2017-11-22 21:07:51 +0100

    test for watchlist

--- src/app/services/watchlist/watchlist.service.spec.ts
+++ src/app/services/watchlist/watchlist.service.spec.ts
@@ -3,7 +3,10 @@ import { TestBed, inject } from '@angular/core/testing'
 import { WatchlistService } from 'services/watchlist/watchlist.service'
 import { HttpModule } from '@angular/http'
 
-describe('UserService', () => {
+describe('WatchlistService', () => {
+  let service: WatchlistService;
+  let htmlElement: HTMLElement;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -17,4 +20,10 @@ describe('UserService', () => {
   it('should be created', inject([WatchlistService], (service: WatchlistService) => {
     expect(service).toBeTruthy()
   }))
+
+  it('should toggle list view button', () => {
+   expect(service.listView).toBeFalsy()
+   service.toggleListView()
+   expect(service.listView).toBeTruthy()
+ })
 })
